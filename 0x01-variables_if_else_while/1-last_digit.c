6638b2368d656c4d708c2a536fef7992f9371b8f
FiifiAnkomah <ankomahjohn46@gmail.com>
2022-10-16 16:08:35 -0700

    task 1 still corrections @ 11:08 pm

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
- *  main - Entry point
- *
- *  Documentation: This program will assign a random number to the variable n each time it is executed
- *
- *  return: always 0 (Success)
+ * main - Entry point
+ * Documentation: This program will assign a random number to the variable n each time it is executed
+ * return: always 0 (Success)
  */
 
 int main(void)
+
 
 {
 	int n;
@@ -24,11 +24,9 @@ int main(void)
 		printf("and is greater than 5\n");
 	
 	else if (n % 10 == 0)
-
 		printf("and is 0\n");
 
 	else if (n % 10 < 6 && n % 10 != 0)
-
 		printf("and is less than 6 and not 0\n");
 	
 	return (0);
