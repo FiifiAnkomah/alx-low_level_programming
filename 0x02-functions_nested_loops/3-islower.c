b6dd1c7c29ee8abfc418f93e57ba6359723bf7ab
FiifiAnkomah <ankomahjohn46@gmail.com>
2022-10-20 19:12:47 -0700

    commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ *
+ * Description: checks is a character is lowercase
+ *
+ * @c: the integer value it receives
+ * Return: 1 if true. 0 if false.
+ */
+
+int_islower(int c)
+{
+	int i = 'a';
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (c == i)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
