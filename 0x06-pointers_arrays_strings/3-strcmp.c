b8d2a627d87d5d28251fa64587e88f8b878c6d34
FiifiAnkomah <ankomahjohn46@gmail.com>
2022-10-27 14:54:29 -0700

    task 3 modified

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,6 +9,7 @@
  * Return: If the strings are equals return "0", if not return other number
  */
 
+
 int _strcmp(char *s1, char *s2)
 {
 	for (; (*s1 != '\0' && *s2 != '\0') && (*s1 == *s2); s1++, s2++)
@@ -18,6 +19,6 @@ int _strcmp(char *s1, char *s2)
 	{
 		return (0);
 	}
-	
 	return (*s1 - *s2);
+
 }
