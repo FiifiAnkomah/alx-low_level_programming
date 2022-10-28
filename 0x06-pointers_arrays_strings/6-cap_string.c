96057c8877b5f8349a38c9c71553fb98a82c47d6
FiifiAnkomah <ankomahjohn46@gmail.com>
2022-10-28 05:55:33 -0700

    modify 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,10 +3,7 @@
 /**
  * cap_string - capitalizes every first letter of a word in a string.
  * @s: pointer to string.
- * separators of words are:  space, tabulation,  new line, ,, ;, ., !, ?,
- * ", (, ), {, and }.
- */
- /* Return: pointer to s. */
+ * Return: pointer to s. */
 
 char *cap_string(char *s)
 {
