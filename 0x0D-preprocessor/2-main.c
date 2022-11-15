3091a836295c2415d9e3c7e0744416c8731aa027
FiifiAnkomah <ankomahjohn46@gmail.com>
2022-11-14 19:54:19 -0800

    commit 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 
 /**
- *main-main function
- *
- *Return: always 0
+ * main-main function
+ * Return: always 0
  */
-
 
 int main(void)
 {
 	printf("%s\n", __FILE__);
-	
 
-return (0);
+	return (0);
 }
